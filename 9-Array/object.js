4b90f14c727a48c798485df61e26c70e9f38c386
Amankhalsa <amansin31@gmail.com>
2023-01-08 22:34:57 +0530

    object find in array

---
+++ 9-Array/object.js
@@ -0,0 +1,15 @@
+
+
+const number = [
+    {name :"kazi" ,age:20},
+    {name :"jack" ,age:30},
+    {name :"raju" ,age:50},
+
+];
+
+
+let result = number.find(function(number){
+
+    return number.age > 30;
+})
+console.log(result);
---
+++ 9-Array/remove.js
@@ -0,0 +1,21 @@
+const  num = [1,2,3,5,2,6]; 
+
+// Remove shift 
+
+// num.shift();
+// console.log(num);
+
+// //Last remove
+
+// num.pop();
+// console.log(num);
+
+// num.splice(1,2);
+// console.log(num);
+
+// ===================== findinf element in array 
+
+console.log(num.includes(2,4));
+console.log(num.indexOf(2));
+
+console.log(num.lastIndexOf(2));
