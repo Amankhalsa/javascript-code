0cb050b45fe5cb7a1aefcad1e9aea21e8e8b71a6
Amankhalsa <amansin31@gmail.com>
2023-01-22 22:53:03 +0530

    trim end start

--- 10-aaray/6-pad.js
+++ 10-aaray/6-pad.js
@@ -1,2 +1,18 @@
-let  min = 4;
-let  hours = 3;
+let  min = '4';
+let  hours = '3';
+
+
+console.log(min.padStart(4,'0'));
+console.log(min.padEnd(4,'0'));
+console.log(hours.padStart(3));
+
+
+let name = '     aryan          ';
+console.log(name);
+
+let res = name.trimStart();
+console.log(res);
+let myvar = name.trimEnd();
+console.log(myvar);
+
+
